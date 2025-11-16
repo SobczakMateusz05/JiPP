b8306baf5db7a7a420e1450949629b8cf9e3bab7
Mateusz Sobczak22 <mateusz.sobczak22@student.pk.edu.pl>
2025-11-16 09:05:30 +0100

    Add lab_4 1. point solution'

---
+++ Lab_4_solution/Lab_4_solution/main.cpp
@@ -0,0 +1,38 @@
+#include <string.h> 
+#include <stdio.h>
+
+void Inp_string(char* str, size_t SilentBytes);
+void Print_string(char* str1, char* str2);
+void Proc_string(char* str1, char str2[], size_t SilentBytes);
+
+int main() {
+	char str1[100];
+	char str2[100];
+
+	Inp_string(str1, sizeof(str1));
+	Inp_string(str2, sizeof(str2));
+	Proc_string(str1, str2, sizeof(str1));
+	Proc_string(str2, str1, sizeof(str2));
+
+	Print_string( str1, str2);
+}
+
+void Inp_string(char* str, size_t SilentBytes) {
+	printf_s("podaj wiersz\n");
+
+	size_t sz = sizeof(str);
+
+	if (gets_s(str, SilentBytes - 1) == NULL) 
+		printf("??? %s\n", str);
+}
+
+void Print_string(char* str1, char* str2) {
+	printf("str1: %s\n", str1);
+	printf("str2: %s\n", str2);
+}
+
+void Proc_string(char* str1, char str2[], size_t SilentBytes) {
+	strcpy_s(str1, SilentBytes, str1);
+	strcat_s(str1, SilentBytes, " ");
+	strcat_s(str1, SilentBytes, str2);
+}
